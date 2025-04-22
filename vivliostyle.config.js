7029d521eed956bc4a2202e5e2698be189dc00b7
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-04-22 11:08:57 +0900

    chore: vivliostyle.config.js の出力設定にPDFを設定

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -14,13 +14,14 @@ module.exports = {
     // 'glossary.html', // html can be passed.
   ], // `entry` can be `string` or `object` if there's only single markdown file.
   // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
-  // output: [ // path to generate draft file(s). default to '{title}.pdf'
-  //   './output.pdf', // the output format will be inferred from the name.
-  //   {
-  //     path: './book',
-  //     format: 'webpub',
-  //   },
-  // ],
+  output: [
+    // path to generate draft file(s). default to '{title}.pdf'
+    "./output.pdf", // the output format will be inferred from the name.
+    //   {
+    //     path: './book',
+    //     format: 'webpub',
+    //   },
+  ],
   workspaceDir: ".vivliostyle", // 一時ファイルの保存場所
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
